b2ed4832529bc3cb3c27e359b26c6be8966f623d
Dawidek <dawidszpener141@gmail.com>
2020-03-21 13:07:35 +0000

    feat: user list is now dynamic by using state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import User from './components/User'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>User List:</h1>
-      <User name = "Dawid" />
-      <User name = "Seba" />
-      <User name = "Marek" />
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [
+      { name: "Dawid" },
+      { name: "Seba" },
+      { name: "Marek" }
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>User List:</h1>
+        <User name = {this.state.users[0].name} />
+        <User name = {this.state.users[1].name} />
+        <User name = {this.state.users[2].name} />
+      </div>
+    );
+  }
 }
 
 export default App;
