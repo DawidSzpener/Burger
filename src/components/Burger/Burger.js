b895487e74b6f79e2017b6d57eee1329cd664730
Dawidek <dawidszpener141@gmail.com>
2020-03-29 21:49:01 +0100

    chore: now burger show a messenge when no ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,18 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-  const newIngredients = Object.keys(props.ingredients)
+  let newIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey}  />
       })
     })
+    .reduce((arr, el) => {
+      return arr.concat(el) 
+    }, [])
+    if (newIngredients.length === 0) { 
+      newIngredients = <p>Please add ingredients</p>
+     }
 
   return (
     <div className={classes.Burger}>
