4f17c5baffdf32cab86d0d9b574de61355cd271c
Dawidek <dawidszpener141@gmail.com>
2020-03-29 22:02:07 +0100

    fix: forgot to give a classname to buildcontrol divs

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import classes from './BuildControl.css'
 
 const buildControl = (props) => (
-  <div>
-    <div>{props.label}</div>
-    <button>Less</button>
-    <button>More</button>
+  <div className={classes.BuildControl}>
+    <div className={classes.Label}>{props.label}</div>
+    <button className={classes.Less}>Less</button>
+    <button className={classes.More}>More</button>
   </div>
 )
 
