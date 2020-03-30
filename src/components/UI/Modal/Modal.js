9ddd76f662130668415e0b2f0538891135f0b7cc
Dawidek <dawidszpener141@gmail.com>
2020-03-30 19:46:29 +0100

    feat: created a modal which displays clients order

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,7 +3,7 @@ import classes from './Modal.css'
 
 const modal = (props) => (
   <div className={classes.Modal}>
-    {props.chidlren}
+    {props.children}
   </div>
 )
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,7 @@ import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 
 const INGREDIENT_PRICES = {
   salad: 0.5, 
@@ -77,7 +78,9 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
-        <Modal />
+        <Modal>
+          <OrderSummary ingredients={this.state.ingredients}/>
+        </Modal>
         <div>
           <Burger ingredients={this.state.ingredients}/>
         </div>
