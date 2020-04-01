d7a4e2dbd658ce02682616a33c60a605a7c39685
Dawidek <dawidszpener141@gmail.com>
2020-04-01 13:52:24 +0100

    chore: adjusted logo height for sidebar and toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,7 +6,7 @@ import NavigationItems from '../NavigationsItems/NavigationItems'
 const toolbar = () => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <Logo />
+    <Logo height="80%"/>
     <nav>
       <NavigationItems />
     </nav>
