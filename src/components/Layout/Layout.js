1a2c8c0937c98441802963693c2787a4a8d68734
Dawidek <dawidszpener141@gmail.com>
2020-04-01 15:05:36 +0100

    chore: sidedrawer now dissapears on a backdrop click

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../../hoc/Aux'
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const Layout = (props) => {
-  return (
-    <Aux>
-      <Toolbar />
-      <SideDrawer />
-      <main className={classes.Content}>
-        {props.children}
-      </main>
-    </Aux>
-  )
+class Layout extends Component  {
+  state = {
+    showSideDrawer: true
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false })
+  }
+
+  render () {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer
+          closed={this.sideDrawerClosedHandler}
+          open={this.state.showSideDrawer}/>
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    )
+  }
 }
 
 export default Layout
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,15 +2,24 @@ import React from 'react'
 import NavigationItems from '../NavigationsItems/NavigationItems'
 import Logo from '../../Logo/Logo'
 import classes from './SideDrawer.css'
+import Backdrop from '../../UI/Backdrop/Backdrop'
+import Aux from '../../../hoc/Aux'
 
 const sideDrawer = (props) => {
+  let attachedClasses = [classes.SideDrawer, classes.Close]
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open]  
+  }
   return (
-    <div className={classes.SideDrawer}>
-      <Logo height="11%"/>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed}/>
+      <div className={attachedClasses.join(' ')}>
+        <Logo height="11%"/>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
   )
 }
 
