91da1e1b5bf7b68b75e295e1ddf1bc9c6e57480d
Dawidek <dawidszpener141@gmail.com>
2020-04-14 19:39:17 +0100

    chore: setting up routing for checkout and main burger page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
+import { Route, Switch } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Switch>
+            <Route path="/"component={BurgerBuilder} />
+            <Route path="/checkout" component={Checkout} />
+          </Switch>
         </Layout>
       </div>
     );
