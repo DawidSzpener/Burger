f6eb1c3b988bce57be7ac591b5c5698ecd0f1b5d
Dawidek <dawidszpener141@gmail.com>
2020-04-15 12:56:41 +0100

    chore: orders page now shows orders from database without ingredients for now

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import classes from './Order.css'
 
-const order = (props) => (
-  <div className={classes.Order}>
-    <p>Ingredients: salad (1)</p>
-    <p>Price: <strong>USD 5.45</strong></p>
-  </div>
-)
+const order = (props) => {
+  const ingredients = []
+
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: salad (1)</p>
+      <p>Price: <strong>{Number.parseFloat(props.price).toFixed(2)}</strong></p>
+    </div>
+  )
+}
 
 export default order
