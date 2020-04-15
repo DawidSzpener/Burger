7cac2e9f9129a56b3692777fab65c3810aaf3c1f
Dawidek <dawidszpener141@gmail.com>
2020-04-15 18:33:03 +0100

    feat: created a custom form component and replaced the basic react one in our app

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from '../../../components/UI/Button/Button'
 import classes from './ContactData.css'
 import axios from '../../../axios-orders'
 import Spinner from '../../../components/UI/Spinner/Spinner'
+import Input from '../../../components/UI/Form/Form'
 
 class ContentData extends Component {
   state = {
@@ -39,10 +40,10 @@ class ContentData extends Component {
   render () {
     let form = (
       <form>
-      <input className={classes.Input} type="text" name="name" placeholder="Your name" />
-      <input className={classes.Input} type="email" name="email" placeholder="Your email" />
-      <input className={classes.Input} type="text" name="street" placeholder="Your street" />
-      <input className={classes.Input} type="text" name="postal" placeholder="Your postal code" />
+      <Input inputtype='input' type="text" name="name" placeholder="Your name" />
+      <Input inputtype='input' type="email" name="email" placeholder="Your email" />
+      <Input inputtype='input' type="text" name="street" placeholder="Your street" />
+      <Input inputtype='input' type="text" name="postal" placeholder="Your postal code" />
       <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
     </form>
     )
