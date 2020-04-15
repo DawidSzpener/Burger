fdc9fab161c1cec52750eafa5b0f5ec6492603da
Dawidek <dawidszpener141@gmail.com>
2020-04-15 12:50:16 +0100

    chore: orders now fetch burgers from firebase database

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react'
 import Order from '../../components/Order/Order'
+import axios from '../../axios-orders'
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true
+  }
+
+  componentDidMount() {
+    axios.get('/burgers.json') 
+      .then(response => {
+        const fetchedData = []
+        for (let key in response.data) {
+          fetchedData.push({
+            ...response.data[key],
+            id: key
+          })
+        }
+        this.setState({ loading: false, orders: fetchedData })
+      })
+      .catch(err => {
+        this.setState({ loading: false })
+      })
+  }
+
   render () {
     return (
       <div>
