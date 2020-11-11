b010a7ad61e759d2dcc4956761ce73b3980431d6
Dawid Szpener <dawidszpener141@gmail.com>
2020-11-11 22:24:09 +0000

    Test navigation items

--- src/components/Navigation/NavigationsItems/NavigationItems.js
+++ src/components/Navigation/NavigationsItems/NavigationItems.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import classes from './NavigationItems.css'
 import NavigationItem from './NavigationItem/NavigationItem'
+import Aux from '../../../hoc/Aux'
 
-const navigationItems = () => (
-  <ul className={classes.NavigationItems}>
-    <NavigationItem link="/">Burger</NavigationItem>
-    <NavigationItem link="/orders">Orders</NavigationItem>
-  </ul>
+const navigationItems = (props) => (
+  <Aux>
+    <ul className={classes.NavigationItems}>
+      <NavigationItem link="/">Burger</NavigationItem>
+      <NavigationItem link="/orders">Orders</NavigationItem>
+      {props.isAuthenticated ? <NavigationItem link='/'>Orders</NavigationItem> : null}
+    </ul>
+  </Aux>
 )
 
 export default navigationItems
--- src/components/Navigation/NavigationsItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationsItems/NavigationItems.test.js
@@ -9,8 +9,21 @@ import NavigationItems from './NavigationItems'
 configure({adapter: new Adapter()})
 
 describe('<NavigationItems/>', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />)
+  })
+
   it('should render two navigation items if not authenticated', () => {
-    const wrapper = shallow(<NavigationItems/>)
     expect(wrapper.find(NavigationItem)).toHaveLength(2)
+  })
+  it('should render three navigation items if authenticated', () => {
+    wrapper.setProps({isAuthenticated: true})
+    expect(wrapper.find(NavigationItem)).toHaveLength(3)
+  })
+  it('should render orders navigation item if authenticated', () => {
+    wrapper.setProps({isAuthenticated: true})
+    expect(wrapper.contains(<NavigationItem link='/'>Orders</NavigationItem>)).toEqual(true)
   })
 })
