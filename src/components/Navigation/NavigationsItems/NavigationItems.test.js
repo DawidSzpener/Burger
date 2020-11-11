0682854f458f3fc2d56c32baaec87e0845eb1d04
Dawid Szpener <dawidszpener141@gmail.com>
2020-11-11 18:43:34 +0000

    Navigation items test

---
+++ src/components/Navigation/NavigationsItems/NavigationItems.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+import NavigationItem from './NavigationItem/NavigationItem'
+import NavigationItems from './NavigationItems'
+
+configure({adapter: new Adapter()})
+
+describe('<NavigationItems/>', () => {
+  it('should render two navigation items if not authenticated', () => {
+    const wrapper = shallow(<NavigationItems/>)
+    expect(wrapper.find(NavigationItem)).toHaveLenght(2)
+  })
+})
